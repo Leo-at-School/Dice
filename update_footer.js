269a7421eff280b60fba7cdce9fb54713a653447
Leo-at-School <153330136+Leo-at-School@users.noreply.github.com>
2025-09-26 09:07:50 -0700

    Update update_footer.js

--- update_footer.js
+++ update_footer.js
@@ -1,4 +1,5 @@
 funtion updateDiceSum(){
+  system.log("hello!!");
   //Reference to the footer where the dice sum is updated (Technically it references a <span> tag within the footer)
   var diceSumFooter = document.getElementById("diceSum");
   
@@ -7,7 +8,9 @@ funtion updateDiceSum(){
   
   //Calls the getDiceSum method inside the Java code that reutrns the dice sum
   var diceSum = canvasReference.getDiceSum();
-
+  
+  system.log(diceSum);
+  
   //Update the footer
   diceSumFooter.innerText = diceSum;
 }
