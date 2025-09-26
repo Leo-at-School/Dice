5b480682294a6da52e4bd11fdc33930850b41fc3
Leo-at-School <153330136+Leo-at-School@users.noreply.github.com>
2025-09-26 00:22:12 -0700

    Update update_footer.js

--- update_footer.js
+++ update_footer.js
@@ -1,6 +1,15 @@
-var randomNumber = Math.random();
-var diceSumFooter = document.getElementById("diceSum");
+funtion updateDiceSum(){
+  //Reference to the footer where the dice sum is updated (Technically it references a <span> tag within the footer)
+  const diceSumFooter = document.getElementById("diceSum");
+  
+  //Instance of the Java code (pde file)
+  const canvasReference = Processing.getInstanceById("Dice");
+  
+  //Calls the getDiceSum method inside the Java code that reutrns the dice sum
+  const diceSum = canvasReference.getDiceSum();
 
-console.log("HI!");
-console.log(diceSumFooter);
-diceSumFooter.innerText = randomNumber;
+  //Update the footer
+  diceSumFooter.innerText = diceSum;
+}
+
+updateDiceSum();
