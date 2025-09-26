bda6982b67d1b3efb39c4eef095e318b44831ad9
Leo-at-School <153330136+Leo-at-School@users.noreply.github.com>
2025-09-25 23:55:56 -0700

    Update update_footer.js

--- update_footer.js
+++ update_footer.js
@@ -1,5 +1,6 @@
 var randomNumber = Math.random();
 var diceSumFooter = document.getElementById("diceSum");
 
+console.log("HI!");
 console.log(diceSumFooter);
 diceSumFooter.innerText = randomNumber;
