4a90080caf77e942e948e58d03338bfc63137618
Leo-at-School <153330136+Leo-at-School@users.noreply.github.com>
2025-09-27 00:31:43 -0700

    Update update_footer.js

--- update_footer.js
+++ update_footer.js
@@ -1,1 +1,8 @@
 var canvasReference = document.getElementById("diceCanvas");
+var footerReference = document.getElementById("diceSum");
+
+canvasReference.addEventListener("click", function(){
+  console.log("HEY!");
+  
+  footerReference.innerText = Math.random();
+});
